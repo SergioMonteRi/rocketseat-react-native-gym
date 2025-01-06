102ad6dd94f227706f6d4b860aabd03f1ff321d4
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-01-06 10:22:29 -0300

    eslint settings

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
-import { StatusBar, Text, View } from 'react-native';
+/* eslint-disable camelcase */
+import { StatusBar, Text, View } from 'react-native'
 
-import { useFonts, Roboto_700Bold, Roboto_400Regular } from "@expo-google-fonts/roboto"
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_700Bold,
-    Roboto_400Regular
+    Roboto_400Regular,
   })
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#202020" }}>
-      <StatusBar barStyle="light-content" backgroundColor={"transparent"} translucent  />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#202020',
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={'transparent'}
+        translucent
+      />
 
       {fontsLoaded ? <Text>Home</Text> : <View />}
     </View>
-  );
+  )
 }
-
